cc6c355fb4f6860169f90dc65460927673f90171
Rohit Nimkar <nehalnimkar@gmail.com>
2022-01-16 09:42:36 +0530

    :tada: add initial code

---
+++ src/main/java/io/csrohit/sockets/SocketsAppApplication.java
@@ -0,0 +1,13 @@
+package io.csrohit.sockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketsAppApplication.class, args);
+	}
+
+}
