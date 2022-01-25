6bfad7f982defd44be52b5e7d8ab69bdf59db30b
Rohit Nimkar <nehalnimkar@gmail.com>
2022-01-25 06:31:02 +0530

    :tada: add angular app

--- src/main/java/io/csrohit/sockets/controller/MainController.java
+++ src/main/java/io/csrohit/sockets/controller/MainController.java
@@ -5,19 +5,29 @@ import io.csrohit.sockets.model.OutputMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller()
-@RequestMapping("**")
+@RequestMapping("")
 public class MainController {
+
+
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
     public OutputMessage send(Message message) throws Exception {
         String time = new SimpleDateFormat("HH:mm").format(new Date());
         return new OutputMessage(message.getFrom(), message.getText(), time);
     }
+
 
 }
